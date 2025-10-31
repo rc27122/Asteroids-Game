d93d9ae6dbd91052b820249199eb657bddf86ae9
rc27122 <171710151+rc27122@users.noreply.github.com>
2025-10-30 23:19:50 -0600

    added player class and controls

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
+from player import Player
+
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill((0, 0, 0))
+        dt = clock.tick(60) / 1000  # compute delta time BEFORE updating the player
+
+        player.update(dt)
+
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
